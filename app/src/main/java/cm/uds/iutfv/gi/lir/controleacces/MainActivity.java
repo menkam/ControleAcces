2de4cf5b205728ef131e3495017ebc3176677d77
menkam <menkam35@gmail.com>
2018-05-30 02:31:56 +0100

    adaptation de la vue tu tuto avec ma vue precedente

--- app/src/main/java/cm/uds/iutfv/gi/lir/controleacces/MainActivity.java
+++ app/src/main/java/cm/uds/iutfv/gi/lir/controleacces/MainActivity.java
@@ -4,18 +4,29 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.github.florent37.materialviewpager.MaterialViewPager;
+
+import cm.uds.iutfv.gi.lir.zxing.integration.android.IntentIntegrator;
+import cm.uds.iutfv.gi.lir.zxing.integration.android.IntentResult;
 
 
 public class MainActivity extends AppCompatActivity  {
@@ -27,7 +38,7 @@ public class MainActivity extends AppCompatActivity  {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main0);
 
         //4 onglets
         final int tabCount = 4;
@@ -42,9 +53,20 @@ public class MainActivity extends AppCompatActivity  {
 
             @Override
             public Fragment getItem(int position) {
-                //je créé pour chaque onglet un RecyclerViewFragment
-                return RecyclerViewFragment.newInstance();
+                switch (position){
+                    case 0:
+                        return RecyclerViewFragment.newInstance();
+                    case 1:
+                        return RecyclerViewFragment.newInstance();
+                    case 2:
+                        return RecyclerViewFragment.newInstance();
+                    case 3:
+                        return RecyclerViewFragment.newInstance();
+                    default:
+                        return null;
+                }
             }
+
 
             @Override
             public int getCount() {
@@ -56,59 +78,83 @@ public class MainActivity extends AppCompatActivity  {
             public CharSequence getPageTitle(int position) {
                 switch (position) {
                     case 0:
-                        return getResources().getString(R.string.divertissement);
+                        return getResources().getString(R.string.etudiant_en_salle);
                     case 1:
-                        return getResources().getString(R.string.sports);
+                        return getResources().getString(R.string.etudiant_ayant_terminer);
                     case 2:
-                        return getResources().getString(R.string.technologie);
+                        return getResources().getString(R.string.etudiant_exclus);
                     case 3:
-                        return getResources().getString(R.string.international);
+                        return getResources().getString(R.string.list_etudiant);
                     default:
                         return "Page " + position;
                 }
             }
 
+
             int oldItemPosition = -1;
+            @SuppressLint("ResourceAsColor")
             @Override
             public void setPrimaryItem(ViewGroup container, int position, Object object) {
                 super.setPrimaryItem(container, position, object);
+
+                FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 
                 //seulement si la page est différente
                 if (oldItemPosition != position) {
                     oldItemPosition = position;
 
                     //définir la nouvelle couleur et les nouvelles images
-                    String imageUrl = null;
+                    Drawable image = null;
                     int color = Color.BLACK;
                     Drawable newDrawable = null;
 
                     switch (position) {
                         case 0:
-                            imageUrl = "http://www.skyscanner.fr/sites/default/files/image_import/fr/micro.jpg";
+                            image = getDrawable(R.drawable.img_en_salle);
                             color = getResources().getColor(R.color.purple);
                             newDrawable = getResources().getDrawable(R.drawable.ticket);
+                            fab.setImageResource(R.color.colorPrimary);
+                            fab.setImageResource(R.drawable.ic_add_user);
+                            fab.setVisibility(View.VISIBLE);
+
                             break;
                         case 1:
-                            imageUrl = "http://www.larousse.fr/encyclopedie/data/images/1311904-Balle_de_tennis_et_filet.jpg";
-                            color = getResources().getColor(R.color.orange);
+                            image = getDrawable(R.drawable.img_terminer);
+                            color = getResources().getColor(R.color.green);
                             newDrawable = getResources().getDrawable(R.drawable.tennis);
+                            fab.setBackgroundColor(R.color.green);
+                            fab.setImageResource(R.drawable.ic_user_finish);
+                            fab.setVisibility(View.VISIBLE);
                             break;
                         case 2:
-                            imageUrl = "http://soocurious.com/fr/wp-content/uploads/2014/03/8-facettes-de-notre-cerveau-qui-ont-evolue-avec-la-technologie8.jpg";
-                            color = getResources().getColor(R.color.cyan);
+                            image = getDrawable(R.drawable.img_tricheur);
+                            color = getResources().getColor(R.color.orange);
                             newDrawable = getResources().getDrawable(R.drawable.light);
+                            fab.setBackgroundResource(R.color.colorBtnRemove);
+                            fab.setImageResource(R.drawable.ic_remove_user);
+                            fab.setVisibility(View.VISIBLE);
                             break;
                         case 3:
-                            imageUrl = "http://graduate.carleton.ca/wp-content/uploads/prog-banner-masters-international-affairs-juris-doctor.jpg";
-                            color = getResources().getColor(R.color.green);
+                            image = getDrawable(R.drawable.img_liste);
+                            color = getResources().getColor(R.color.cyan);
                             newDrawable = getResources().getDrawable(R.drawable.earth);
+                            fab.setVisibility(View.INVISIBLE);
                             break;
                     }
 
+                    fab.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            scanCode();
+                            //setTabEnCours(tab.getPosition());
+                        }
+                    });
+
                     //puis modifier les images/couleurs
-                    int fadeDuration = 400;
+                    int fadeDuration = 200;
                     materialViewPager.setColor(color, fadeDuration);
-                    materialViewPager.setImageUrl(imageUrl, fadeDuration);
+                    //materialViewPager.setImageUrl(imageUrl, fadeDuration);
+                    materialViewPager.setImageDrawable(image, fadeDuration);
                     toggleLogo(newDrawable,color,fadeDuration);
 
                 }
@@ -158,6 +204,86 @@ public class MainActivity extends AppCompatActivity  {
 
         //démarre l'animation de disparition
         animatorSetDisappear.start();
+    }
+
+    private void scanCode() {
+        //scan
+        IntentIntegrator scanIntegrator = new IntentIntegrator(this);
+        scanIntegrator.initiateScan();
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        //retrieve scan result
+        IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        if (scanningResult != null) {
+            //we have a result
+            String scanContent = scanningResult.getContents();
+            String scanFormat = scanningResult.getFormatName();
+
+            if(scanFormat != null && scanContent!= null){
+                //if(getTabEnCours()==0) EtudiantsEnSalle.addStudent(getApplicationContext(), scanFormat, scanContent);
+                //if(getTabEnCours()==1) EtudiantsAyantTerminer.addStudent(getApplicationContext(), scanFormat, scanContent);
+                //if(getTabEnCours()==2) EtudiantsExclus.addStudent(getApplicationContext(), scanFormat, scanContent);
+            }
+            else Toast.makeText(getApplicationContext(), "No scan data received!", Toast.LENGTH_SHORT).show();
+        }
+        else Toast.makeText(getApplicationContext(), "No scan data received!", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
 }
